e4acbf18037b66b156f4e19439877e19f369f47e
matthieu.begoghina <12561988+Voulz@users.noreply.github.com>
2025-11-28 18:54:57 +1100

    Update of Target.cs files for 5.7

--- Source/TouchEngineUESamp.Target.cs
+++ Source/TouchEngineUESamp.Target.cs
@@ -8,7 +8,7 @@ public class TouchEngineUESampTarget : TargetRules
 	public TouchEngineUESampTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.Add("TouchEngineUESamp");
 	}
 }
--- Source/TouchEngineUESampEditor.Target.cs
+++ Source/TouchEngineUESampEditor.Target.cs
@@ -8,7 +8,7 @@ public class TouchEngineUESampEditorTarget : TargetRules
 	public TouchEngineUESampEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
         ExtraModuleNames.Add("TouchEngineUESamp");
 
         // ~ IncludeOrderVersion ~
